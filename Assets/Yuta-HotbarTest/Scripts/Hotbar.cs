b69a2ee60131a03173c1291423ca3adb2e4b5ab2
Fumihiro1 <105392494+Fumihiro1@users.noreply.github.com>
2023-10-01 15:12:06 +1300

    Comments

--- Assets/Yuta-HotbarTest/Scripts/Hotbar.cs
+++ Assets/Yuta-HotbarTest/Scripts/Hotbar.cs
@@ -7,25 +7,35 @@ using UnityEngine.UI;
 
 public class Hotbar : MonoBehaviour
 {
+    //List to store items in the hotbar
     public List<itemType> hotbarList = new List<itemType>();
+
+    //Serialized game objects for different item types
     [SerializeField] GameObject sword;
     [SerializeField] GameObject pickaxe;
     [SerializeField] GameObject axe;
+
+    //Index of the current selected item
     public int selectedItem;
 
+    // Dictionary to map item types to their respective GameObjects
     private Dictionary<itemType, GameObject> setActive = new Dictionary<itemType, GameObject>() { };
 
     private void Start()
     {
+        //Initialize Dictionary by mapping item types to the game objects
         setActive.Add(itemType.Sword, sword);
         setActive.Add(itemType.Pickaxe, pickaxe);
         setActive.Add(itemType.Axe, axe);
 
+        //Call the method to set the initial selected item
         NewItemSelected();
     }
 
     private void Update()
     {
+        //Check for keypress. Currently for 123, call the method to update the selected item's visibility
+        //in the game
         if (Input.GetKeyDown(KeyCode.Alpha1) && hotbarList.Count > 0) {
             selectedItem = 0;
             NewItemSelected();
@@ -43,11 +53,13 @@ public class Hotbar : MonoBehaviour
     }
     private void NewItemSelected()
     {
+        //Deactivate all gameobject items by default
         sword.SetActive(false);
         pickaxe.SetActive(false);
         axe.SetActive(false);
 
-        GameObject GOSelected = setActive[hotbarList[selectedItem]];
-        GOSelected.SetActive(true);
+        //Activate the selected item from the hotbar list at corresponding index
+        GameObject Selected = setActive[hotbarList[selectedItem]];
+        Selected.SetActive(true);
     }
 }
--- Assets/Yuta-HotbarTest/Scripts/ItemAsset.cs
+++ Assets/Yuta-HotbarTest/Scripts/ItemAsset.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Create more items
 [CreateAssetMenu(fileName = "Hotbar Item", menuName = "Hotbar Item/Create Item")]
 public class ItemAsset : ScriptableObject
 {
-    public itemType item_type;
-    public Sprite item_sprite;
+    public itemType item_type; //Item type
+    public Sprite item_sprite; //For UI element
 }
 
+//Enum of the items available. 
 public enum itemType { Sword, Pickaxe, Axe };
 
