23309ba7f9f5e0214d00b5379258b92c8f92cdfd
Yuta Yamagai <105392494+Fumihiro1@users.noreply.github.com>
2023-10-19 15:14:43 +1300

    Uploading progress
    Overall unit testing feature seems to be working..? No bugs??

---
+++ Assets/Scripts/Editmode/SavingMechanic.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class SavingMechanic
+{
+    [Test]
+    public void TestLoad()
+    {
+
+    }
+
+    [Test]
+    public void TestSave()
+    {
+
+    }
+}
